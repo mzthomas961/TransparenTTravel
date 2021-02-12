8f6970cf8feead6b844f7373a8ebdaf0b0c2dd08
github username <github email address>
2021-02-12 13:59:33 -0500

    progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+#######Agent data######
+mike = Agent.create(name:"Mike")
+matt = Agent.create(name:"Matt")
+
+
+########Client data######
+eric = Client.create(name: "Eric", age: 30, agent_id: 1)
+chase = Client.create(name: "Chase", age: 36, agent_id: 2 )
+
+
+#########Hotel data#######
